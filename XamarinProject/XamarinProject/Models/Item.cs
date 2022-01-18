d71835bdab4a3e44bbd8e6bb3867d6d054276d27
email <mdargouthi0@gmail.com>
2022-01-18 15:25:43 +0100

    First commit

---
+++ XamarinProject/XamarinProject/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XamarinProject.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string resName { get; set; }
+        public string Location { get; set; }
+    }
+}
